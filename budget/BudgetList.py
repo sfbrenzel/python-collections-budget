83b1fa52120a9e9fd3c9dadc883182b2b6e907c4
sfbrenzel <sfbrenzel@gmail.com>
2021-12-07 15:29:54 -0500

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,6 @@
-from . import Expense
+import matplotlib.pyplot as plt
+from. import Expense
 
-# Class that extends list type
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
@@ -12,9 +12,9 @@ class BudgetList():
     def __len__(self):
         return (len(self.expenses) + len(self.overages))
 
-    # implement append so that it only appends to self if total < budget
+
     def append(self, item):
-        # TODO Check if item is a number
+
         if (self.sum_expenses+item < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item
@@ -23,21 +23,41 @@ class BudgetList():
             self.overages.append(item)
             self.sum_overages+=item
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+
 
 
 def main():
-    # Using above class
-    # Set starting budget to 500
+
     myBudgetList = BudgetList(1200)
-    # Add expenses, the last expense is 100 and that goes in overages
+
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
-    # Test len()
+
     print('The count of all expenses: ' + str(len(myBudgetList)))
 
+    for entry in myBudgetList:
+        print(entry)
+
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
 if __name__ == "__main__":
     main()
