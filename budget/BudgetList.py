08b4449387a74f5dfc8b3c05599f250a1e70eef2
sfbrenzel <sfbrenzel@gmail.com>
2021-12-07 14:34:23 -0500

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,13 @@
 from . import Expense
 
- class BudgetList():
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+for expense in expenses.list:
+    expense.amount.append(myBudgetList)
+
+class BudgetList():
+    """docstring forBudgetList."""
+
     def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
@@ -8,28 +15,21 @@ from . import Expense
         self.sum_overages = 0
         self.overages = []
 
-    def __len__(self):
-        return (len(self.expenses) + len(self.overages))
-
     def append(self, item):
-        if (self.sum_expenses+item < self.budget):
-            self.expenses.append(item)
-            self.sum_expenses += item
-        else:
-            self.overages.append(item)
-            self.sum_overages+=item
+            if (self.sum_expenses + item < self.budget):
+                self.expenses.append(item)
+                self.sum.expenses + item
+            else
+                self.overages.append(item)
+                self.sum.overages + item
 
+    def __len__(self):
+        return (self.expenses + self.sum_overages)
 
+def main():
+    myBudgetList = BudgetList(1200)
 
- def main():
-     myBudgetList = BudgetList(1200)
-     expenses = Expense.Expenses()
-     expenses.read_expenses('data/spending_data.csv')
-     for expense in expenses.list:
-         myBudgetList.append(expense.amount)
+    print('the count of all expenses:' + str(len(myBudgetList)))
 
-     print('The count of all expenses: ' + str(len(myBudgetList)))
-
-
- if __name__ == "__main__":
-     main()
+    if __name__ == "__main__"
+        main()
